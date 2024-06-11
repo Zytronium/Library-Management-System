4d243fb2a80f01627c7b332786d7bcc9f13bb362
Zytronium <90411599+Zytronium@users.noreply.github.com>
2024-06-11 16:34:03 -0500

    initial commit

---
+++ books_database.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+typedef struct {
+
+
+};
---
+++ user_database.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+struct User {
+	char *name[25];
+	int *DOB[3]; /* date of brith | { Month, Day, Year } */
+	int userID;
+
+};
